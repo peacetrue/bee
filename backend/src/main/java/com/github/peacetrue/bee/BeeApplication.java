f47cb7e02c241456c2d6bdd0ff6ad1dcc934f1e5
peacetrue <xiayouxue@gmail.com>
2022-08-18 20:46:46 +0800

    搭建应用

---
+++ backend/src/main/java/com/github/peacetrue/bee/BeeApplication.java
@@ -0,0 +1,16 @@
+package com.github.peacetrue.bee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Bee 应用。
+ *
+ * @author peace
+ **/
+@SpringBootApplication
+public class BeeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BeeApplication.class, args);
+    }
+}
