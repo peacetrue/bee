0a12e3fab372973e3050bb0cab1f3359b644c645
peacetrue <xiayouxue@gmail.com>
2022-08-18 16:43:20 +0800

    实现首页布局

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {AppBar, Container, CssBaseline, Grid, Link, Toolbar, Typography} from "@mui/material";
+import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
 
+//@formatter:off
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline/>
+      <Container maxWidth={false}>
+        <AppBar>
+          <Toolbar variant="dense">
+            <Link href="https://bee.peacetrue.cn" color="inherit"><EmojiNatureIcon/></Link>
+            <Typography variant="h6" color="inherit" component="div" ml={1}>Bee</Typography>
+            <Grid container ml={4} spacing={4}>
+              <Grid item>
+                <Link href="https://peacetrue.github.io/bee" color="inherit" underline="none" target={"_blank"}>文档</Link>
+              </Grid>
+              <Grid item>
+                <Link href="https://peacetrue.github.io/bee" color="inherit" underline="none">监控</Link>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+        <Grid container flexDirection={"column"} minHeight={"100vh"}>
+          <Grid item>
+            <Toolbar/>
+          </Grid>
+          <Grid item flexGrow={1}
+                container flexDirection={"column"}
+                justifyContent={"center"} alignItems={"center"} spacing={4} >
+            <Grid item><Typography variant={"h1"}>Bee</Typography></Grid>
+            <Grid item><Typography variant={"h3"}>开发者助手</Typography></Grid>
+            <Grid item><Typography variant={"h5"}>辅助技术人员日常开发，致力于在琐碎的细节中提升效率</Typography></Grid>
+          </Grid>
+          <Grid item textAlign={"center"} height={"80px"}>
+            © 2022 peacetrue. 鄂ICP备20006312号
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 }
 
