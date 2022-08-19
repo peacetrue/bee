0c0f7a618479169909361f794745a97b5cd047ab
peacetrue <xiayouxue@gmail.com>
2022-08-19 15:33:53 +0800

    打包

--- backend/src/main/java/com/github/peacetrue/bee/BeeApplication.java
+++ backend/src/main/java/com/github/peacetrue/bee/BeeApplication.java
@@ -3,6 +3,8 @@ package com.github.peacetrue.bee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 /**
  * Bee 应用。
  *
