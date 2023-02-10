80e7d6a2d7f268f0887c3cafdd2815b2442f4312
peacetrue <xiayouxue@gmail.com>
2023-02-10 17:56:23 +0800

    macOS

---
+++ env-init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+pwd=$(pwd)
+# 根据 Shell 环境变量，初始化 workflows 环境变量
+cd "$pwd/.github/workflows" || exit
+back_envs=(DEV LOCAL PROD)
+for app in FRONTEND MAIN MONITOR ; do
+    for env in "${back_envs[@]}" ; do
+      name="BEE_${app}_URL_${env}"
+      value=$(printenv "$name")
+      sed -i "s|${name}.*|${name}: $value|g" macOS.yml
+    done
+done
+
+# 根据 Shell 环境变量，初始化文档环境变量
+cd "$pwd/docs/antora" || exit
+sed -i "/BEE/d" "antora.yml"
+for env in $(printenv | grep BEE | grep -v BEE_REMOTE) ; do
+    sed -i "/start_page/i${env//=/: }" "antora.yml"
+done
+#https://unix.stackexchange.com/questions/552689/multiline-variable-add-a-tab-before-each-newline
+(TAB=$'    ' ; sed -i "s/^BEE/${TAB}BEE/" "antora.yml")
+
+# 根据 Shell 环境变量，初始化前端环境变量
+cd "$pwd/frontend" || exit
+#https://stackoverflow.com/questions/1494178/how-to-define-hash-tables-in-bash
+#https://stackoverflow.com/questions/6047648/associative-arrays-error-declare-a-invalid-option
+#https://www.cnblogs.com/yy3b2007com/p/11267237.html
+front_envs=(development native production)
+for (( i = 0; i < ${#front_envs[@]}; i++ )); do
+  front_env=${front_envs[i]}
+  back_env=${back_envs[i]}
+  sed -i "s|REACT_APP_API_URL.*|REACT_APP_API_URL='$(printenv "BEE_MAIN_URL_${back_env}")'|g" ".env.$front_env"
+  sed -i "s|REACT_APP_SWAGGER_URL.*|REACT_APP_SWAGGER_URL='$(printenv "BEE_MAIN_URL_${back_env}")/swagger-ui/index.html'|g" ".env.$front_env"
+  sed -i "s|REACT_APP_MONITOR_URL.*|REACT_APP_MONITOR_URL='$(printenv "BEE_MONITOR_URL_${back_env}")'|g" ".env.$front_env"
+done
